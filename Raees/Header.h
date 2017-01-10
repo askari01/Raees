0252c6592e3c462f387e75e3f5d837d68db73f91
Farrukh Askari <farrukh.askari01@gmail.com>
2017-01-10 16:38:34 +0500

    Initial Setup

---
+++ Raees/Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  Raees
+//
+//  Created by Syed Askari on 1/10/17.
+//  Copyright © 2017 Syed Askari. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+#import "WikitudeSDK/WTArchitectView.h"
+
+
+#endif /* Header_h */
